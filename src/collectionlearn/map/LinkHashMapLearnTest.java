817540669671206cd78de66ee11c782fe98aad7c
beastmouth <hbj@33.cn>
2019-01-12 16:47:23 +0800

    linkedHashMap learn

---
+++ src/collectionlearn/map/LinkHashMapLearnTest.java
@@ -0,0 +1,17 @@
+package collectionlearn.map;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author huang
+ * @version 1.0
+ * @date 2019/01/11 16:51
+ **/
+
+public class LinkHashMapLearnTest {
+    public static void main(String[] args) {
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("aaa", "aaa");
+        linkedHashMap.put("bbb", "bbb");
+    }
+}
