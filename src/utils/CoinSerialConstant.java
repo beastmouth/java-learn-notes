4a0650864f45a509de4c1fa3a9c4accb738d006b
beastmouth <hbj@33.cn>
2019-01-15 20:47:36 +0800

    拆分int类型为char数组

---
+++ src/utils/CoinSerialConstant.java
@@ -0,0 +1,20 @@
+package utils;
+
+/**
+ * @author huang
+ * @version 1.0
+ * @date 2019/01/15 16:56
+ **/
+
+public class CoinSerialConstant {
+    public static final int ZERO_ASCII = 48;
+    public static final int ONE_ASCII = 49;
+    public static final int TWO_ASCII = 50;
+    public static final int THREE_ASCII = 51;
+    public static final int FOUR_ASCII = 52;
+    public static final int FIVE_ASCII = 53;
+    public static final int SIX_ASCII = 54;
+    public static final int SEVEN_ASCII = 55;
+    public static final int EIGHT_ASCII = 56;
+    public static final int NINE_ASCII = 57;
+}
---
+++ src/utils/CoinSerialNumberUtil.java
@@ -0,0 +1,70 @@
+package utils;
+
+import java.util.Arrays;
+
+/**
+ * @author huang
+ * @version 1.0
+ * @date 2019/01/15 16:31
+ **/
+
+public class CoinSerialNumberUtil {
+    public static String createCoinSerialNumber(Integer billId) {
+        String billNum = String.valueOf(billId);
+        StringBuilder sb = new StringBuilder();
+        char[] billNums = billNum.toCharArray();
+        System.out.println(Arrays.toString(billNums));
+        for (char num : billNums) {
+            switch (num) {
+                case CoinSerialConstant.ZERO_ASCII :
+                    sb.append("A");
+                    break;
+                case CoinSerialConstant.ONE_ASCII :
+                    sb.append("B");
+                    break;
+                case CoinSerialConstant.TWO_ASCII :
+                    sb.append("C");
+                    break;
+                case CoinSerialConstant.THREE_ASCII :
+                    sb.append("D");
+                    break;
+                case CoinSerialConstant.FOUR_ASCII :
+                    sb.append("E");
+                    break;
+                case CoinSerialConstant.FIVE_ASCII :
+                    sb.append("F");
+                    break;
+                case CoinSerialConstant.SIX_ASCII :
+                    sb.append("G");
+                    break;
+                case CoinSerialConstant.SEVEN_ASCII :
+                    sb.append("H");
+                    break;
+                case CoinSerialConstant.EIGHT_ASCII :
+                    sb.append("I");
+                    break;
+                case CoinSerialConstant.NINE_ASCII :
+                    sb.append("J");
+                    break;
+                default:
+                    sb.append("K");
+            }
+        }
+        String result = sb.toString();
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+//        createCoinSerialNumber(1234684695);
+        int i = 0;
+        while (true) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("1111");
+        }
+    }
+}
