0201fb2e9f75fa0e811302f26f89f68fdf89b3d0
beastmouth <hbj@33.cn>
2019-01-28 18:48:35 +0800

    tesst

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+        int i = 1;
+        System.out.println(i+++i++);
+        int a = i;
+        System.out.println(a);
+        System.out.println(i);
     }
 }
