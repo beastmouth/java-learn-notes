5876366728918574ad1e15fb85cdd9c061472669
beastmouth <hbj@33.cn>
2019-02-12 20:26:05 +0800

    mmmm

--- src/main/hellogo.go
+++ src/main/hellogo.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/BeastMouth/java-learn-notes/src/imgo"
+
+	//"github.com/BeastMouth/java-learn-notes/src/imgo"
 )
 
 const (
@@ -41,34 +43,39 @@ func swapReturn (x,y string) (string,string) {
 }
 
 func main ()  {
-	fmt.Println("Hello world!")
-	imgo.HelloGo()
-	var age int
-	age = 23
-	fmt.Printf("my age is %d \n",age)
+	//fmt.Println("Hello world!")
+	//imgo.HelloGo()
+	//var age int
+	//age = 23
+	//fmt.Printf("my age is %d \n",age)
+	//
+	////const 常量 不再被修改的量
+	//const gl string  = "gl"
+	//fmt.Println(gl)
+	//
+	////println 打印的位置是不固定的 类似于开了多线程打印一样
+	////要保证顺序  fmt.Println
+	//println(aaa,bbb)
+	//fmt.Println(aaa, bbb)
+	//fmt.Println(aa,bb,cc,dd,ee,ff,gg,hh,ii)
+	//fmt.Println(a,b,c,d)
+	//
+	//var str1 = "str1"
+	//var str2 = "str2"
+	//swap(str1, str2)
+	//fmt.Println(str1, str2)
+	//
+	//// := 声明并赋值  声明的变量不能再之前声明过
+	//str3, str4 := swapReturn(str1, str2)
+	//fmt.Println(str3,str4)
+	//
+	//imgo.PointGo()
+	//
+	//hbj := imgo.SetPersonInfo("hbj",23, "男")
+	//fmt.Println(hbj)
 
-	//const 常量 不再被修改的量
-	const gl string  = "gl"
-	fmt.Println(gl)
+	var name string
+	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+	fmt.Printf("Hello, %v \n", name)
 
-	//println 打印的位置是不固定的 类似于开了多线程打印一样
-	//要保证顺序  fmt.Println
-	println(aaa,bbb)
-	fmt.Println(aaa, bbb)
-	fmt.Println(aa,bb,cc,dd,ee,ff,gg,hh,ii)
-	fmt.Println(a,b,c,d)
-
-	var str1 = "str1"
-	var str2 = "str2"
-	swap(str1, str2)
-	fmt.Println(str1, str2)
-
-	// := 声明并赋值  声明的变量不能再之前声明过
-	str3, str4 := swapReturn(str1, str2)
-	fmt.Println(str3,str4)
-
-	imgo.PointGo()
-
-	hbj := imgo.SetPersonInfo("hbj",23, "男")
-	fmt.Println(hbj)
 }
