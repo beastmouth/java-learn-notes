25681e05f50e8762fbd5e367f0eff6d7cb4b7c38
beastmouth <hbj@33.cn>
2019-02-12 20:24:59 +0800

    mmm

--- src/imgo/sort/InsertionSort.go
+++ src/imgo/sort/InsertionSort.go
@@ -1,13 +1,17 @@
 package sort
 
-func InsertionSort(numbers []int) []int {
-	length := len(numbers)
-	var temp int
-	for i := 1; i < length; i++ {
-		for j := 0; j < i; j++ {
-			if numbers[i] < numbers[j] {
+import "fmt"
 
-			}
-		}
-	}
+func InsertionSort(numbers []int)  {
+	//length := len(numbers)
+	//var temp int
+	//for i := 1; i < length; i++ {
+	//	for j := 0; j < i; j++ {
+	//		if numbers[i] < numbers[j] {
+	//
+	//		}
+	//	}
+	//}
+	//return numbers
+	fmt.Printf("hello")
 }
