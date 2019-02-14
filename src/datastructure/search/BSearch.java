28d42f60fe7e803e272f0686c46d8747c1d78d2e
beastmouth <hbj@33.cn>
2019-02-14 16:55:55 +0800

    二分查找变形问题

--- src/datastructure/search/BSearch.java
+++ src/datastructure/search/BSearch.java
@@ -14,7 +14,16 @@ public class BSearch {
         while (low <= high) {
             int mid = low + ((high - low) >> 1);
             if (array[mid] == value) {
-                return mid;
+                //原来的代码
+                //return mid;
+
+                //找重复数字的最后一个(不是最后一个就往后移动)
+                if ((mid == n - 1) || (array[mid + 1] != value)) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+
             } else if (array[mid] > value) {
                 high = mid - 1;
             } else {
@@ -29,7 +38,7 @@ public class BSearch {
         return bSearchInternally(array, 0, array.length - 1, value);
     }
 
-    public static int bSearchInternally(int[] array, int low, int high, int value) {
+    private static int bSearchInternally(int[] array, int low, int high, int value) {
         if (low > high) {
             return -1;
         }
@@ -40,6 +49,85 @@ public class BSearch {
             return bSearchInternally(array, low, mid - 1, value);
         } else {
             return bSearchInternally(array, mid + 1, high, value);
+        }
+    }
+
+    /**
+     * 二分查找变形问题
+     * */
+    public static int bSearch3(int[] array, int n, int value) {
+        //寻找第一个大于给定元素的数
+        return bSearchInternally2(array, 0, n - 1, value);
+    }
+
+    /**
+     * 二分查找变形问题
+     * */
+    private static int bSearchInternally2(int[] array, int low, int high, int value) {
+        //寻找第一个大于给定元素的数
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + ((high - low) >> 1);
+        //原先自己写的
+//        if (array[mid] > value) {
+//            if (array[mid - 1] >= value) {
+//                return bSearchInternally2(array, low, mid - 1, value);
+//            } else {
+//                return mid;
+//            }
+//        } else {
+//            return bSearchInternally2(array, mid + 1, high, value);
+//        }
+        if (array[mid] >= value) {
+            //之前少了个判断条件 当他为第一个元素的时候 mid-1 等于 -1 超出数组的表示范围了
+            if (mid == 0 || array[mid - 1] < value) {
+                return mid;
+            } else {
+                return bSearchInternally2(array, low, mid - 1, value);
+            }
+        } else {
+            return bSearchInternally2(array, mid + 1, high, value);
+        }
+    }
+
+    /**
+     * 二分查找变形问题
+     * */
+    public static int bSearch4(int[] array, int n, int value) {
+        //寻找最后一个小于等于给定元素的数
+        return bSearchInternally3(array, 0, n - 1, value);
+    }
+
+    /**
+     * 二分查找变形问题
+     * */
+    private static int bSearchInternally3(int[] array, int low, int high, int value) {
+        //寻找最后一个小于等于给定元素的数
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + ((high - low) >> 1);
+//        原来写的
+//        if (array[mid] >= value) {
+//            return bSearchInternally3(array, low, mid - 1, value);
+//        } else {
+//            if (array[mid + 1] <= value) {
+//                return bSearchInternally3(array, mid + 1, high, value);
+//            } else {
+//                return mid;
+//            }
+//        }
+        if (array[mid] <= value) {
+            if (mid == array.length - 1 || array[mid + 1] > value) {
+                return mid;
+            } else {
+                return bSearchInternally3(array, mid + 1, high, value);
+            }
+        } else {
+            return bSearchInternally3(array, mid + 1, high, value);
         }
     }
 }
--- src/datastructure/search/TestSearch.java
+++ src/datastructure/search/TestSearch.java
@@ -8,8 +8,8 @@ package datastructure.search;
 
 public class TestSearch {
     public static void main(String[] args) {
-        int[] numbers = new int[]{1, 2, 32, 44, 55, 456, 900};
-        int result = BSearch.bSearch2(numbers, numbers.length, 3);
+        int[] numbers = new int[]{1, 2, 32, 44, 44, 44, 55, 456, 900};
+        int result = BSearch.bSearch4(numbers, numbers.length, 900);
         System.out.println(result);
     }
 }
