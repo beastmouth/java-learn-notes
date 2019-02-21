1afaea39f156cec8f0f9bc74f91a5ae4051c2062
beastmouth <hbj@33.cn>
2019-02-21 11:17:38 +0800

    bsearch leetcode unfinish

--- src/datastructure/search/BSearchLeetCode.java
+++ src/datastructure/search/BSearchLeetCode.java
@@ -29,53 +29,157 @@ package datastructure.search;
 
 public class BSearchLeetCode {
     public static void main(String[] args) {
-        int[] numbers = new int[]{4,5,6,7,0,1,2};
+//        int[] numbers = new int[]{4,5,6,7,0,1,2};
+//        int[] numbers = new int[]{7,0,1,2,4,5,6};
+//        int[] numbers = new int[]{6, 7, 0, 1, 2, 4, 5, 6};
+//        int[] numbers = new int[]{3, 1};
+//        System.out.println(search(numbers, 1));
     }
 
+//    private static int search(int[] nums, int target) {
+//        int low = 0;
+//        int high = nums.length - 1;
+//        int divideIndex = searchDivideIndex(nums, low, high);
+//        if (divideIndex < 0) {
+//            return -1;
+//        }
+//        if (target > nums[low]) {
+//            if (divideIndex > 0) {
+//                return searchNumIndex(nums, low, (divideIndex - 1), target);
+//            } else {
+//                return searchNumIndex(nums, divideIndex, high, target);
+//            }
+//        } else {
+//            return searchNumIndex(nums, divideIndex, high, target);
+//        }
+//    }
+//
+//    private static int searchDivideIndex(int[] nums, int low, int high) {
+//        if (low > high) {
+//            return -1;
+//        }
+//        if (nums[low] < nums[high]) {
+//            return 0;
+//        }
+//        int mid = low + ((high - low) >> 1);
+//        if (nums[mid] < nums[low]) {
+//            if (mid == low) {
+//                return mid;
+//            } else {
+//                if (nums[mid] > nums[mid - 1]) {
+//                    return searchDivideIndex(nums, low, mid - 1);
+//                } else {
+//                    return mid;
+//                }
+//            }
+//        } else {
+//            if (mid == high) {
+//                return mid;
+//            } else {
+//                if (nums[mid] < nums[mid + 1]) {
+//                    return searchDivideIndex(nums, mid + 1, high);
+//                } else {
+//                    return mid;
+//                }
+//            }
+//        }
+//    }
+//
+//    private static int searchNumIndex(int[] nums, int low, int high, int target) {
+//        if (low > high) {
+//            return -1;
+//        }
+//        int mid = low + ((high - low) >> 1);
+//        if (nums[mid] == target) {
+//            return mid;
+//        } else if (nums[mid] > target) {
+//            return searchNumIndex(nums, low, mid - 1, target);
+//        } else {
+//            return searchNumIndex(nums, mid + 1, high, target);
+//        }
+//    }
 
-    public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        int mid = low + ((high - low) >> 1);
-        if (nums[mid] == target) {
-            return mid;
-        } else if (low < high) {
-            //有序的情况
-            return searchOrderly(nums, low, high, target);
-        } else {
-            //无序的情况
-            return -1;
-        }
-    }
+//    public int search(int[] nums, int target) {
+//        int low = 0;
+//        int high = nums.length - 1;
+//        int mid = low + ((high - low) >> 1);
+//        if (nums[mid] == target) {
+//            return mid;
+//        } else {
+//            return searchTarget(nums, low, high, target);
+//        }
+//        else if (low < high) {
+//            //有序的情况
+//            return searchOrderly(nums, low, high, target);
+//        } else {
+//            //无序的情况
+//            return searchUnOrderly(nums, low, high, target);
+//        }
+//    }
 
-    private int searchOrderly(int[] nums, int low, int high, int target) {
-        if (low > high) {
-            return -1;
-        }
-        int mid = low + ((high - low) >> 1);
-        if (nums[mid] == target) {
-            return mid;
-        } else if (nums[mid] > target) {
-            return searchOrderly(nums, low, mid - 1, target);
-        } else {
-            return searchOrderly(nums, mid + 1, high, target);
-        }
-    }
+//    private int searchTarget(int[] nums, int low, int high, int target) {
+//        if (low > high) {
+//            return -1;
+//        }
+//        int mid = low + ((high - low) >> 1);
+//        if (nums[mid] == target) {
+//            return mid;
+//        } else {
+//            if (nums[mid] > nums[low]) {
+//                if (nums[mid] > target) {
+//                    return searchTarget(nums, low, mid - 1, target);
+//                } else {
+//                    return searchTarget(nums, mid + 1, high, target);
+//                }
+//            } else {
+//                if (nums[mid] < nums[high]) {
+//
+//                } else {
+//
+//                }
+//            }
+//        }
+//    }
 
-    private int searchUnOrderly(int[] nums, int low, int high, int target) {
-        if (low > high) {
-            return -1;
-        }
-        int mid = low + ((high - low) >> 1);
-        if (nums[mid] == target) {
-            return mid;
-        } else {
-            if (nums[mid] > nums[low] && nums[low] < target) {
-                searchOrderly(nums, low, mid - 1, target);
-            } else if (nums[mid] < nums[high] && nums[high] > target) {
-                searchOrderly(nums, mid + 1, high, target);
-            }
-        }
-    }
+//    private int searchOrderly(int[] nums, int low, int high, int target) {
+//        if (low > high) {
+//            return -1;
+//        }
+//        int mid = low + ((high - low) >> 1);
+//        if (nums[mid] == target) {
+//            return mid;
+//        } else if (nums[mid] > target) {
+//            return searchOrderly(nums, low, mid - 1, target);
+//        } else {
+//            return searchOrderly(nums, mid + 1, high, target);
+//        }
+//    }
+//
+//    private int searchUnOrderly(int[] nums, int low, int high, int target) {
+//        if (low > high) {
+//            return -1;
+//        }
+//        int mid = low + ((high - low) >> 1);
+//        if (nums[mid] == target) {
+//            return mid;
+//        } else {
+////            if (nums[mid] > nums[low] && nums[low] < target) {
+////                searchOrderly(nums, low, mid - 1, target);
+////            } else if (nums[mid] < nums[high] && nums[high] > target) {
+////                searchOrderly(nums, mid + 1, high, target);
+////            }
+//            if (nums[mid] > nums[low]) {
+//
+//            } else {
+//
+//            }
+//            if (nums[mid] < nums[high]) {
+//
+//            } else {
+//
+//            }
+//
+//        }
+//    }
 
 }
