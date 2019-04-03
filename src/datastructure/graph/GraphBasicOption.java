123df73b2053fb611852e8daadbc8a68b0e7d755
hbj <hbj@33.cn>
2019-04-03 19:56:00 +0800

    图 未开始

---
+++ src/datastructure/graph/GraphBasicOption.java
@@ -0,0 +1,18 @@
+package datastructure.graph;
+
+
+
+/**
+ * @author huang
+ * @version 1.0
+ * @date 2019/04/03 16:54
+ **/
+
+public class GraphBasicOption {
+
+}
+
+class GraphNode{
+    private String uid;
+    private GraphNode next;
+}
